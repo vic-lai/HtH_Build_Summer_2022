7a3195c69dd43b18cd0b6766285a7b2cf7d44657
victor lai <laivictor80@gmail.com>
2022-07-05 17:20:28 -0700

    assignment 1

---
+++ Getting-Started-with-Python-viclai/about_me.py
@@ -0,0 +1,22 @@
+full_name = "Victor Lai"
+nickname = "Victor"
+age = 19
+used_python = True
+
+hobbies = [
+  "video games", "lifting", "watching streams", "watching anime"
+]      
+
+favorite_things = {
+  "video game":"Valorant", 
+  "anime": "Hunter x Hunter", 
+  "Hobby": hobbies[1]
+}
+
+print(full_name, nickname, age, used_python)
+print()
+print(hobbies)
+print()
+print(favorite_things)
+
+all_vars = dict(vars())
